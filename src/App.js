867dcf594ea4cbe7302d97d717d49b539ff05e62
arslanah99 <arah09@hotmail.com>
2020-07-29 16:22:21 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Accordion, Card } from "react-bootstrap";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [teamData, setTeamData] = useState([]);
+
+  const nbaData = async () => {
+    const response = await axios.get(
+      "https://www.balldontlie.io/api/v1/teams/"
+    );
+
+    setTeamData(response.data.data);
+  };
+
+  const renderAccordion = (team, index) => {
+    return (
+      <Accordion key={index}>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey={team}>
+            {team.city} <i>+</i>
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey={team}>
+            <Card.Body>
+              <ul>
+                <li>{team.abbreviation}</li>
+                <li>{team.city}</li>
+                <li>{team.conference}</li>
+                <li>{team.division}</li>
+                <li>{team.full_name}</li>
+              </ul>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+    );
+  };
+
+  useEffect(() => {
+    nbaData();
+  }, []);
+
+  return <div className="App">{teamData.map(renderAccordion)}</div>;
+};
 
 export default App;
